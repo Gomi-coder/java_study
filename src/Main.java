ebb0fc7c085b3937654ed38fc27bf8b6f668fd9d
태고밍 <68893329+TaeKyeong-coder@users.noreply.github.com>
2024-02-20 03:20:34 +0900

    자바의 주석

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // TODO : learn Java's annotation
+        // 한 줄 짜리 주석
+        /*
+        여러 줄 짜리 주석
+        */
+
         System.out.println("Hello and welcome!");
         //println => end 값에 \n print => ''
 
