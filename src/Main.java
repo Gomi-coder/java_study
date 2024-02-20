d6c691c1aa286e6dc500954bb7551d03d94eeda2
태고밍 <68893329+TaeKyeong-coder@users.noreply.github.com>
2024-02-20 20:39:33 +0900

    print println printf

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,48 @@ public class Main {
         여러 줄 짜리 주석
         */
 
-        System.out.println("Hello and welcome!");
-        //println => end 값에 \n print => ''
+        // TODO : how to print
+        // ( ) 안의 내용을 출력하고 줄바꿈
+        System.out.println("안녕하세요");
+        System.out.println("반갑습니다");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        // ( ) 안의 내용을 출력. 줄바꿈 없음.
+        System.out.print("오늘 날씨는 ");
+        System.out.print("흐림");
+
+        // 커서 위치 다음 줄로 옮기기
+        System.out.println();
+        System.out.println("줄바꿈 확인");
+
+        // 숫자 출력 : 따옴표로 묶지 않아도 출력할 수 있음
+        System.out.println(100);
+
+        // TODO : 출력 서식을 가진 출력문
+        // 정수 출력
+        System.out.printf("정수 : %d\n", 100);
+        System.out.printf("정수 : %d %d\n", 200, 300);
+        // 자리수 채우기
+        System.out.printf("%10d\n", 10);
+        System.out.printf("%010d\n", 10);
+        // 정수 범위 확인
+        System.out.printf("%d ~ %d\n", -2147483648, 2147483647);
+        // Error
+        // System.out.printf("%d ~ %d\n", -2147483649, 2147483648);
+        // 더 많은 숫자를 수용할 수 있게 더 넓은 기억 공간 잡기
+        System.out.printf("%d ~ %d\n", -2147483649L, 2147483648L);
+        System.out.printf("%d ~ %d\n", -9223372036847758081L, 9223372036847750807L);
+
+        // 문자 찍기 (문자 한 개 => 작은 따옴표)
+        System.out.printf("%c", 'a');
+
+        // 실수 찍기
+        System.out.printf("%f\n", 123.456);
+        // 정수 부분은 제어가 되지 않음. 소수점 부분만 제어가 됨.
+        System.out.printf("%2.2f\n", 123.456);
+        System.out.printf("%.2f\n", 123.456);
+
+        // 문자열
+        System.out.printf("%s", "문자열 출력");
+        System.out.printf("%s %d\n", "문자열과 정수열 섞어서 출력", 200);
     }
 }
